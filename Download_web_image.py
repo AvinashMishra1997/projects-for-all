81aaccf63aa67eb43d32fb30f3d71e1d5eb29a43
AvinashMishra1997 <avinashmishra1997@gmail.com>
2017-01-18 18:52:26 +0530

    Update Download_web_image.py

--- Download_web_image.py
+++ Download_web_image.py
@@ -2,9 +2,9 @@ import random
 import urllib
 
 x=raw_input('Enter url here:')
-def download_url_lib(url):
+def download_web_image(url):
     name= random.randrange(1,1000)
     full_name= str(name) + '.jpg'
     urllib.urlretrieve(url , full_name)
 
-download_url_lib(x)
+download_web_image(x)
