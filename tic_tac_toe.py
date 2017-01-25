4c2ed92d105c0cd2091d4a8e0a7e45f0502cd138
AvinashMishra1997 <avinashmishra1997@gmail.com>
2017-01-25 23:38:47 +0530

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,40 @@
+a='   '
+b='   '
+c='   '
+d='   '
+e='   '
+f='   '
+g='   '
+h='   '
+i='   '
+
+
+t=('\t'+a+'|'+b+'|'+c+'\n\t___________\n\t'+d+'|'+e+'|'+f+'\n\t___________\n\t'+g+'|'+h+'|'+i )
+print t
+for unknowns in t:
+    unknowns= raw_input('Enter column number and row number here without spaces in between: ')
+    u_s= str(unknowns)
+    if u_s == '11':
+        a=raw_input('Enter x or o:  ')
+    elif u_s == '12':
+        b=raw_input('Enter x or o:  ')
+    elif u_s == '13':
+        c=raw_input('Enter x or o:  ')
+    elif u_s == '21':
+        d=raw_input('Enter x or o:  ')
+    elif u_s == '22':
+        e=raw_input('Enter x or o:  ')
+    elif u_s== '23':
+        f=raw_input('Enter x or o:  ')
+    elif u_s == '31':
+        g=raw_input('Enter x or o:  ')
+    elif u_s == '32':
+        h=raw_input('Enter x or o:  ')
+    elif u_s == '33':
+        i=raw_input('Enter x or o:  ')
+    else:
+        print ('INPUT ERROR:')
+    t1=('\t'+a+'|'+b+'|'+c+'\n\t___________\n\t'+d+'|'+e+'|'+f+'\n\t___________\n\t'+g+'|'+h+'|'+i )
+    t=t1
+
+    print (t)
